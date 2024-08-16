12886165d19608c5be0658f9237e42972c8a6c4a
Andrew-Neely-82 <andrew.neely82@gmail.com>
2024-08-15 22:57:59 -0500

    project started / jframe and panel added

---
+++ src/main/GameWindow.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.swing.*;
+
+public class GameWindow {
+  private JFrame jFrame;
+
+  public GameWindow(GamePanel gamePanel) {
+    final int width = 400;
+    final int height = 400;
+
+    jFrame = new JFrame();
+    jFrame.setSize(width, height);
+    jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    jFrame.add(gamePanel);
+    jFrame.setVisible(true);
+  }
+}

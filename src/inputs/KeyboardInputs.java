a0f0341cbe1fda9c97b0750961812397559d22bb
Andrew-Neely-82 <andrew.neely82@gmail.com>
2024-08-15 23:57:49 -0500

    added mouse movements

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -1,10 +1,16 @@
 package inputs;
 
 
+import main.GamePanel;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyboardInputs implements KeyListener {
+  private GamePanel gamePanel;
+  public KeyboardInputs(GamePanel gamePanel) {
+    this.gamePanel = gamePanel;
+  }
 
   @Override
   public void keyTyped(KeyEvent e) {
@@ -13,10 +19,10 @@ public class KeyboardInputs implements KeyListener {
   @Override
   public void keyPressed(KeyEvent e) {
     switch (e.getKeyCode()) {
-      case KeyEvent.VK_W: System.out.println("W Pressed"); break;
-      case KeyEvent.VK_A: System.out.println("A Pressed"); break;
-      case KeyEvent.VK_S: System.out.println("S Pressed"); break;
-      case KeyEvent.VK_D: System.out.println("D Pressed"); break;
+      case KeyEvent.VK_W: gamePanel.changeYDelta(-5); break;
+      case KeyEvent.VK_A: gamePanel.changeXDelta(-5); break;
+      case KeyEvent.VK_S: gamePanel.changeYDelta(5); break;
+      case KeyEvent.VK_D: gamePanel.changeXDelta(5); break;
     }
   }
 
